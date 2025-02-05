fb58e872bfc4718abfe20172d4ed0ae1c5095ace
Jenq-q <adhikarijenish623@gmail.com>
2025-02-05 23:22:50 +0545

    AddedGuns
    Bang bang

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    public float range = 15f;
+    string enemyTag = "Enemy";
+    public Transform turretHead;
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceFromEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceFromEnemy < shortestDistance)
+            {
+                shortestDistance = distanceFromEnemy;
+                nearestEnemy = enemy;
+            }
+            if(nearestEnemy != null && shortestDistance <= range) 
+            {
+                target = nearestEnemy.transform;
+            }
+            else
+            {
+                target = null;
+            }
+        }
+    }
+    private void Update()
+    {
+        if (target == null) return;
+
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = lookRotation.eulerAngles;
+        turretHead.rotation = Quaternion.Euler(0f, rotation.y,  0f);
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
